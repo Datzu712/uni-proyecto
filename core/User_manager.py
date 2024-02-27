2b38dc3e04d54d99b9ecd359d824ca1ece354080
gabovalle <gabrielvalleborquet02@gmail.com>
2024-02-26 23:17:28 -0600

    Se agrego el manager para los paquetes, facturas electronicas y usuarios

--- core/User_manager.py
+++ core/User_manager.py
@@ -1,3 +1,6 @@
+from .Input_helper import pedir_datos
+from ..structures.User import User
+
 class User_manager:
     
     def __init__(self):
@@ -5,5 +8,13 @@ class User_manager:
         
     def add_user(self, user):
         self.users.append(user)
+
+    def registrar_usuario(self):
+        new_user = pedir_datos(User)
+        self.add_user(new_user)
+        
+
+
+
 
 
