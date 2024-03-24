4e9f9f5c8dff0f101ba95d06bee049875bc6394f
AaronUlate <aaron.ulate@gmail.com>
2024-03-24 13:33:48 -0600

    Se agrega la opcion de cambiar el estado de los paquetes

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ guias = []
 num_autoincremental = 0
 
 while True:
-    print(packages)
-    print(guias)
+    
     print("Bienvenido a la Mensajería Fidélitas\n")
     print("1. Ingresar un nuevo usuario")
     print("2. Ingresar un nuevo paquete")
-    print("3. Ingresar una nueva factura")
+    print("3. Cambiar estado de un paquete")
+    print("4. Ingresar una nueva factura")
     print("4. Salir\n")
    
     option = input()
@@ -42,6 +42,7 @@ while True:
         new_package = gen_input_to_list(package_questions)
         clear_console()
         print('[CREACION DE LA GUIA]')
+    
         new_guia = gen_input_to_list(guia_questions)
         num_autoincremental += 1
 
@@ -57,8 +58,38 @@ while True:
         guias.append(new_guia)
         clear_console()
         print('[PAQUETE CREADO]')
+        print("El paquete se ha creado con el numero de guia: ", num_autoincremental)
 
-    elif option == "3":     
+    elif option == "3":
+        
+        if len(packages) == 0:
+            print("No hay paquetes")
+            continue
+        try:
+            numero_guia = int(input("Ingrese el numero de guía: "))
+        except ValueError as error:
+            print("Ingreso un numero invalido")
+            continue  
+        reference_package = None
+        for package in packages:
+            if numero_guia == package[6]:
+                reference_package = package
+        if package == None:
+            print("No se ha encontrado el paquete indicado")
+        else:
+            package_status = input('Eliga un estado entre (Creado-Recolectado-Entrega Fallida-Entregado): ').upper().strip()
+            if package_status not in ['CREADO','RECOLECTADO','ENTREGAFALLIDA','ENTREGADO']:
+                print('Ha escrito una opción incorrecta')
+                continue
+            package[5] = package_status
+            print(package[5])
+            
+            
+            
+
+        
+
+    elif option == "4":     
         clear_console()
         print('[CREACION DE FACTURA ELECTRONICA]')
         new_electronic_bill = gen_input_to_list(electronic_bill_questions)
