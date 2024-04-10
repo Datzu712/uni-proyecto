910d3201bc86550362a50581a4bdf9eaa43b36d4
AaronUlate <aaron.ulate@gmail.com>
2024-04-09 20:36:18 -0600

    Se agrega la opcion de Rastrear un paquete

--- main.py
+++ main.py
@@ -28,8 +28,9 @@ while True:
     print("1. Ingresar un nuevo usuario")
     print("2. Ingresar un nuevo paquete")
     print("3. Cambiar estado de un paquete")
-    print("4. Ingresar una nueva factura")
-    print("5. Salir\n")
+    print("4. Rastrear un paquete")
+    print("5. Ingresar una nueva factura")
+    print("6. Salir\n")
    
     option = input()
    
@@ -89,9 +90,27 @@ while True:
                 continue
             package[5] = package_status
             print('Estado cambiado con exito')
-        
 
-    elif option == "4":     
+    elif option == "4":
+        
+        if len(packages) == 0:
+            print("No hay paquetes")
+            continue
+        try:
+            numero_guia = int(input("Ingrese el numero de guía: "))
+        except ValueError as error:
+            print("Ingreso un numero invalido")
+            continue  
+        reference_package = None
+        for package in packages:
+            if numero_guia == package[6]:
+                reference_package = package
+        if package == None:
+            print("No se ha encontrado el paquete indicado")
+        else:
+            print('El Estado de su paquete es', package[5])    
+
+    elif option == "5":     
         clear_console()
         print('[CREACION DE FACTURA ELECTRONICA]')
         new_electronic_bill = gen_input_to_list(electronic_bill_questions)
@@ -99,7 +118,7 @@ while True:
         clear_console()
         print('[FACTURA ELECTRONICA CREADA]')
         
-    elif option == '5':
+    elif option == '6':
         print('Saliendo...')
         break
     else:
