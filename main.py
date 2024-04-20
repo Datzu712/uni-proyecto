493b92c6d2fa6edfc01e4b6609e84838a4b7fee4
Datzu712 <juan@meddyg.com>
2024-04-19 20:07:42 -0600

    code refactor/bug fix

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ USERS_FILE_PATH = './data/users.json'
 PACKAGES_FILE_PATH = './data/packages.json'
 BILLS_FILE_PATH = './data/bills.json'
 GUIAS_FILE_PATH = './data/guias.json'
+USERS_FILE_PATH = './data/users.json'
 
 def clear_console():
     command = 'clear'
@@ -19,25 +20,16 @@ def clear_console():
         command = 'cls'
     os.system(command)
 
-users = load_file(USERS_FILE_PATH)
-packages = load_file(PACKAGES_FILE_PATH)
-bills = load_file(BILLS_FILE_PATH)
-guias = load_file(GUIAS_FILE_PATH)
-
-
 def user_creation():
-    
-    USERS_FILE_PATH = './data/users.json'
-    users = []
+    users = load_file(USERS_FILE_PATH)
     encontrado = False
     while not encontrado:
-        users = load_file(USERS_FILE_PATH)
-        print('Seleccione una opcion antes de comenzar \n1.Iniciar sesion en su cuenta \n2.Registrar una nueva cuenta')
+        print('Seleccione una opcion antes de comenzar \n1.Iniciar sesión en su cuenta \n2.Registrar una nueva cuenta')
         option = int(input())
         if option == 1:
-            usuario = input('Porfavor Ingrese el correo electronico asociado a su cuenta: ')
+            target_email = input('Porfavor Ingrese el correo electrónico asociado a su cuenta: ')
             for user in users:
-                if usuario == user[0]:
+                if target_email == user[0]:
                     print(f'Usuario seleccionado: {user[0]}')
                     password = input('Ingrese la contraseña de la cuenta: ')
                     print(user[5])
@@ -45,13 +37,14 @@ def user_creation():
                         encontrado = False
                         print('Inicio de sesión exitoso')
                         app(user[6])
-            print('Usuario no encontrado')
+            print('Usuario no encontrado o contraseña incorrecta')
 
         elif option == 2:
             clear_console()
             print('[CREACION DE USUARIO]')
             new_user = gen_input_to_list(user_questions)
             id = random.randint(100000, 999999)
+
             new_user.append(id)
             users.append(new_user)
             save_data(USERS_FILE_PATH, users)
@@ -60,13 +53,13 @@ def user_creation():
             print('[USUARIO CREADO]')
            
 
-def app(user):
-    users = load_file(USERS_FILE_PATH)
+def app(user_id):
     packages = load_file(PACKAGES_FILE_PATH)
     bills = load_file(BILLS_FILE_PATH)
     guias = load_file(GUIAS_FILE_PATH)
     guias = load_file(GUIAS_FILE_PATH)
     num_autoincremental = 0
+
     for guia in guias:
         if guia[5] > num_autoincremental:
             num_autoincremental = guia[5]
@@ -74,7 +67,6 @@ def app(user):
     print(num_autoincremental)
     clear_console()
     while True:
-        print(users)
         print(packages)
         print(bills)
         print(guias)
@@ -108,8 +100,8 @@ def app(user):
             # Se le agrega la ID de la guia
             new_package.append(num_autoincremental)
 
-            new_package.append(user)
-            new_guia.append(user)
+            new_package.append(user_id)
+            new_guia.append(user_id)
 
             packages.append(new_package)
             guias.append(new_guia)
@@ -129,21 +121,24 @@ def app(user):
             try:
                 numero_guia = int(input("Ingrese el numero de guía: "))
             except ValueError as error:
-                print("Ingreso un numero invalido")
-                continue  
+                clear_console()
+                print("Ha ingresado un numero invalido")
+                continue
+
             reference_package = None
             for package in packages:
                 if numero_guia == package[6]:
                     reference_package = package
-            if package == None:
+    
+            if reference_package == None:
                 print("No se ha encontrado el paquete indicado")
             else:
-                package_status = input('Eliga un estado entre (Creado-Recolectado-Entrega Fallida-Entregado): ').upper().replace(' ','')
+                package_status = input('Elija un estado entre (Creado-Recolectado-Entrega Fallida-Entregado): ').upper().replace(' ','')
                 print(package_status)
                 if package_status not in ['CREADO','RECOLECTADO','ENTREGAFALLIDA','ENTREGADO']:
                     print('Ha escrito una opción incorrecta')
                     continue
-                package[5] = package_status
+                reference_package[5] = package_status
 
                 save_data(PACKAGES_FILE_PATH, packages)
                 print('Estado cambiado con exito')
@@ -156,22 +151,23 @@ def app(user):
                 numero_guia = int(input("Ingrese el numero de guía: "))
             except ValueError as error:
                 print("Ingreso un numero invalido")
-                continue  
+                continue 
             reference_package = None
             for package in packages:
                 if numero_guia == package[6]:
                     reference_package = package
-            if package == None:
+
+            if reference_package == None:
                 print("No se ha encontrado el paquete indicado")
             else:
-                print('El Estado de su paquete es', package[5])    
+                print('El Estado de su paquete es', reference_package[5])    
 
         elif option == "4": 
             bills = load_file(BILLS_FILE_PATH)
             clear_console()
             print('[CREACION DE FACTURA ELECTRONICA]')
             new_electronic_bill = gen_input_to_list(electronic_bill_questions)
-            new_electronic_bill.append(user)
+            new_electronic_bill.append(user_id)
             bills.append(new_electronic_bill)
             save_data(BILLS_FILE_PATH, bills)
             clear_console()
@@ -187,53 +183,58 @@ def app(user):
                 packages = load_file(PACKAGES_FILE_PATH)
                 print('Elija una de las siguientes opciones \n1.Mostrar total de envios realizados \n2.Lista de paquetes enviados \n3.Monto de cobro \n4.Cantidad de paquetes enviados por número de telefono \n5.Cantidad de envios por numero de cédula \n6. Salir del modulo de estadisticas')
                 elegir = int(input())
-                
                 if elegir == 1:
                     paquete_suma = 0
                     for paquete in packages:
-                        if paquete[7] == user:
+                        if paquete[7] == user_id:
                             paquete_suma+=1
+                    clear_console()
                     print(f'El total de paquetes enviados es {paquete_suma}')
 
                 elif elegir == 2:
-                    for paquete in packages:
-                        if paquete[7] == user:
-                            print(paquete,'\n')
+                    user_packages = []
+                    for package in packages:
+                        if paquete[7] == user_id:
+                            user_packages.append(package)
+                    clear_console()
+                    if len(user_packages) == 0:
+                        print('No se encontraron paquetes enviados')
+                        continue
+
+                    print('Lista de paquetes enviados')
+                    for package in user_packages:
+                        print(package)
 
                 elif elegir == 3:
                     numero_guia = int(input('Ingrese el numeron de guia a consultar:'))
                     for guia in guias:
                         if numero_guia == guia[5]:
-                             print(f'El monto de cobro de la guia {numero_guia} es: {guia[4]}')
+                            clear_console()
+                            print(f'El monto de cobro de la guia {numero_guia} es: {guia[4]}')
                 elif elegir == 4:
                     numero_telefono = input('Ingrese el numero telefonico, para consultar la cantidad de paquetes encontrados bajo ese numero bajo su cuenta: ')
                     suma_telefono = 0
                     for paquete in packages:
-                        if paquete[7] == user and paquete[1] == numero_telefono:
-                            suma_telefono+=1 
+                        if paquete[7] == user_id and paquete[1] == numero_telefono:
+                            suma_telefono+=1
+                    clear_console()
                     print(f'El total de paquetes enviados con el numero de telefono {numero_telefono} es {suma_telefono}')
 
                 elif elegir == 5:
                     numero_cedula = input('Ingrese el numero de cedula, para consultar la cantidad de paquetes encontrados bajo esa cedula bajo su cuenta: ')
                     suma_cedula = 0
                     for paquete in packages:
-                        if paquete[7] == user and paquete[2] == numero_cedula:
-                            suma_cedula+=1 
-                    print(f'El total de paquetes enviados con el numero de telefono {numero_cedula} es {suma_cedula}')
-                            
-                            
+                        if paquete[7] == user_id and paquete[2] == numero_cedula:
+                            suma_cedula+=1
+                    clear_console()
+                    print(f'El total de paquetes enviados con el numero de telefono {numero_cedula} es {suma_cedula}')  
                 elif elegir == 5:
                     print(1)
                 elif elegir == 6:
                     print('Saliendo del modulo de estadísticas')
                     salir = True 
-
                 else: 
                     print('Porfavor seleccione una opción valida')
-
-                
-
-            
         elif option == '6':
             print('Saliendo...')
             break
@@ -244,3 +245,5 @@ try:
     user_creation()
 except KeyboardInterrupt:
     print('Saliendo...')
+except ValueError as error:
+    print('Ha digitado un valor incorrecto, por favor intente de nuevo...')
